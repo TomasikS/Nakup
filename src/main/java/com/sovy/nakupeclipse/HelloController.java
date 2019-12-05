22bad3db66166b41698ddf45fd10630714338ea6
unknown <stefan.tomasik@synapse.com>
2019-12-05 10:22:27 +0100

    first commit

---
+++ src/main/java/com/sovy/nakupeclipse/HelloController.java
@@ -0,0 +1,51 @@
+package com.sovy.nakupeclipse;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.ws.rs.*;
+
+/**
+ *
+ */
+@Path("/hello")
+@Singleton
+public class HelloController {
+
+    @GET
+    public String sayHello() {
+        return "Hello World";
+    }
+
+
+
+    @Inject
+    NakupDao nakupDao = new NakupDao();
+
+
+    //@GET
+    // public String sayHello() {
+    //   return "Hello World";
+    //}
+
+
+    @GET
+    @Path("/Nakup/{id}")
+    public Nakup getNakupById(@PathParam("id") Long id) {
+        return nakupDao.getNakupById(id);
+    }
+
+    @POST
+    @Path("/add/")
+    @Consumes("application/json")
+    public void addNakup(Nakup nakup) {
+        nakupDao.saveNakup(nakup);
+    }
+
+    @DELETE
+    @Path("/del/{id}")
+    public void deleteNakupById(@PathParam("id") int id) {
+        nakupDao.deleteNakup(id);
+    }
+
+
+}
---
+++ src/main/java/com/sovy/nakupeclipse/Nakup.java
@@ -0,0 +1,83 @@
+package com.sovy.nakupeclipse;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "nakup")
+public class Nakup implements Serializable {
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Nakup() {}
+
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+
+
+  @Column(name = "datumObjednavky")
+  private String datumObjednavky;
+
+  public String getDatumDodania() {
+    return datumDodania;
+  }
+
+  public void setDatumDodania(String datumDodania) {
+    this.datumDodania = datumDodania;
+  }
+
+  @Column(name = "datumDodania")
+  private String datumDodania;
+
+  public String getDatumObjednavky() {
+    return datumObjednavky;
+  }
+
+  public void setDatumObjednavky(String datumObjednavky) {
+    this.datumObjednavky = datumObjednavky;
+  }
+
+  @Column(name = "datumSplatnosti")
+  private String datumSplatnosti;
+
+    public String getDatumSplatnosti() {
+        return datumSplatnosti;
+    }
+
+    public void setDatumSplatnosti(String datumSplatnosti) {
+        this.datumSplatnosti = datumSplatnosti;
+    }
+
+
+
+
+
+    @Column(name = "sposobPlatby")
+    private String sposobPlatby;
+
+  public String getSposobPlatby() {
+    return sposobPlatby;
+  }
+
+  public void setSposobPlatby(String sposobPlatby) {
+    this.sposobPlatby = sposobPlatby;
+  }
+
+
+
+  /*public Nakup(String firstName, String lastName, String email, String platba) {
+    this.datumDodania = firstName;
+    this.datumObjednavky = lastName;
+    this.datumSplatnosti = email;
+    this.sposobPlatby = platba;
+  }*/
+}
