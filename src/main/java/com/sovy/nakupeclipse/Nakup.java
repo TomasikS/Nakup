acd8edbf6a4bb7fa127c85dea862d072d1f58bc0
unknown <stefan.tomasik@synapse.com>
2020-01-10 14:12:25 +0100

    changes in model

--- src/main/java/com/sovy/nakupeclipse/Nakup.java
+++ src/main/java/com/sovy/nakupeclipse/Nakup.java
@@ -7,47 +7,46 @@ import java.io.Serializable;
 @Table(name = "nakup")
 public class Nakup implements Serializable {
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public Nakup() {}
+    public Nakup() {
+    }
 
-  @Id
-  @Column(name = "id")
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "datumObjednavky")
+    private String datumObjednavky;
 
+    public String getDatumDodania() {
+        return datumDodania;
+    }
 
-  @Column(name = "datumObjednavky")
-  private String datumObjednavky;
+    public void setDatumDodania(String datumDodania) {
+        this.datumDodania = datumDodania;
+    }
 
-  public String getDatumDodania() {
-    return datumDodania;
-  }
+    @Column(name = "datumDodania")
+    private String datumDodania;
 
-  public void setDatumDodania(String datumDodania) {
-    this.datumDodania = datumDodania;
-  }
+    public String getDatumObjednavky() {
+        return datumObjednavky;
+    }
 
-  @Column(name = "datumDodania")
-  private String datumDodania;
+    public void setDatumObjednavky(String datumObjednavky) {
+        this.datumObjednavky = datumObjednavky;
+    }
 
-  public String getDatumObjednavky() {
-    return datumObjednavky;
-  }
-
-  public void setDatumObjednavky(String datumObjednavky) {
-    this.datumObjednavky = datumObjednavky;
-  }
-
-  @Column(name = "datumSplatnosti")
-  private String datumSplatnosti;
+    @Column(name = "datumSplatnosti")
+    private String datumSplatnosti;
 
     public String getDatumSplatnosti() {
         return datumSplatnosti;
@@ -57,24 +56,40 @@ public class Nakup implements Serializable {
         this.datumSplatnosti = datumSplatnosti;
     }
 
-
-
-
-
     @Column(name = "sposobPlatby")
     private String sposobPlatby;
 
-  public String getSposobPlatby() {
-    return sposobPlatby;
-  }
+    public String getSposobPlatby() {
+        return sposobPlatby;
+    }
 
-  public void setSposobPlatby(String sposobPlatby) {
-    this.sposobPlatby = sposobPlatby;
-  }
+    public void setSposobPlatby(String sposobPlatby) {
+        this.sposobPlatby = sposobPlatby;
+    }
 
+    public Long getIdUser() {
+        return idUser;
+    }
 
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
 
-  /*public Nakup(String firstName, String lastName, String email, String platba) {
+    public Long getIdBook() {
+        return idBook;
+    }
+
+    public void setIdBook(Long idBook) {
+        this.idBook = idBook;
+    }
+
+    @Column(name = "idUser")
+    private Long idUser;
+
+    @Column(name = "idBook")
+    private Long idBook;
+
+    /*public Nakup(String firstName, String lastName, String email, String platba) {
     this.datumDodania = firstName;
     this.datumObjednavky = lastName;
     this.datumSplatnosti = email;
